9ff093e6ac9aed715fedbbdb285428e9bb6543ab
Thummawit <zzc0420285@celestica.com>
2021-10-01 17:32:52 +0700

    finish solution

--- string2int/string2int/Program.cs
+++ string2int/string2int/Program.cs
@@ -6,7 +6,23 @@ namespace string2int
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter string : ");
+            string input = Console.ReadLine();
+            Console.WriteLine(String2Int(input));
+        }
+
+        static int String2Int(string str)
+        {
+            int value = 0;
+            foreach(char c in str)
+            {
+                if(c >= 48 && c <= 57)
+                {
+                value = value * 10 + (c - '0');
+                }
+            }
+
+            return value;
         }
     }
 }
